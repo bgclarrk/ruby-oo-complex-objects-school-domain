a79bda7a1b48a4deaf44bca03d54aa89ac42f5c1
+ <+>
2020-11-26 09:25:42 -0700

    commit tree eae27fc493201adaef964672686da0da89b3ecd2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @student_name = name
+        @student_grade = grade
+        if @roster[@student_grade] == nil
+            @roster[@student_grade] = []
+            @roster[@student_grade] << "#{@student_name}"
+        else
+            @roster[@student_grade] << "#{@student_name}"
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+end
